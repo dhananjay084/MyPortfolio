b08c73d3b93f5b84c25bb9d74d61d41d566b3173
Dhananjay Bansal <dhananjaybansal28@gmail.com>
2025-01-08 02:47:49 +0530

    first commit

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         attachments: [
           {
             filename: 'resume.pdf',
-            path: 'https://dhananjay-one.vercel.app/resume.pdf', // Ensure this file exists in the public folder
+            path: 'https://dhananjay84.vercel.app/resume.pdf', // Ensure this file exists in the public folder
           },
         ],
       });
